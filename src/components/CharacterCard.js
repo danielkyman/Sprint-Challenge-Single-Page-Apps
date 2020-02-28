ed7332778afb865a9f1491f5ffb269d8343d0f92
daniel kyman <danny.kyman@gmail.com>
2020-02-28 10:51:35 -0800

    Completed MVP

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,17 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <div className="list">
+      {props.data.map(d => (
+        <div className="card" key={d.id}>
+          <img src={d.image} alt="" />
+          <h2>{d.name}</h2>
+          <p>{d.status}</p>
+          <p>{d.species}</p>
+          <p>{d.gender}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,46 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from "./CharacterCard";
+import SearchForm from "./SearchForm";
+import axios from "axios";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+
+  const [data, setData] = useState([]);
+  // const [names, setNames] = useState("");
+
+
+  const search = name => {
+    console.log(name);
+    const newData = data.filter(info => {
+      return info.name === name;
+    }); setData(newData);
+  }
+
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios
+      .get(`https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/`)
+      .then(response => {
+        setData(response.data.results)
+      })
+      .catch(error => {
+        console.log("Data not returned - ", error);
+      });
+  }, []);;
+
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <SearchForm search={search} />
+      <CharacterCard data={data} />
     </section>
   );
 }
+
+
+// id={char.id}
+//             name={char.name}
+//             status={char.status}
+//             species={char.species}
+//             gender={char.gender}
+//             image={char.image}
